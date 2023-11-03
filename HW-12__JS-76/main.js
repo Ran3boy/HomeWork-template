4f7d71064c4fb1be8268149944dd676c76693563
Ran3boy <144589575+Ran3boy@users.noreply.github.com>
2023-11-04 00:16:10 +0500

    Add files via upload

---
+++ HW-12__JS-76/main.js
@@ -0,0 +1,17 @@
+// const firstName = 'John'
+// const lastName = 'Smith'
+// var age = 25
+
+// console.log(firstName, lastName, age, 'y.o')
+
+// age = 30
+
+// =====================ДЗ========================
+
+const firstName = 'Nikolay'
+const lastName = 'Ivanov'
+let age = 20
+let city = 'Orenburg'
+let rise = 190
+
+console.log(firstName, lastName, age, 'y.o', city, rise)
