436459d1d36138c633a897b5ac5c6d9e7fe268a0
Ran3boy <144589575+Ran3boy@users.noreply.github.com>
2023-11-05 22:05:47 +0500

    Add files via upload

---
+++ HW-12__JS-81/main.js
@@ -0,0 +1,30 @@
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers[4] = 6
+
+// console.log(numbers[4])
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i] + 1)
+// }
+
+// =====================ДЗ==========================
+
+const numbers = [1, 3, 6, 7, 10, 12, 15, 18, 20, 21]
+
+for (let i = 0; i <= numbers.length; i++) {
+    if (numbers[i] % 2 == 0)
+    console.log(numbers[i])
+}
+console.log('')
+console.log('1 сделано')
+console.log('')
+
+
+const letters = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый']
+
+for (let a = 6; a >= 0; a--) {
+    console.log(letters[a])
+}
+console.log('')
+console.log('2 сделано')
